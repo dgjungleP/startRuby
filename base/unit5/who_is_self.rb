7c94825cb818ac06f55e42c018087a7acc4806a9
jungle <dgjungle_c@163.com>
2021-10-06 23:55:53 +0800

    unit 5

---
+++ base/unit5/who_is_self.rb
@@ -0,0 +1,20 @@
+puts "Top Level"
+puts "Self is #{self}"
+
+class C
+  puts "Class definition block:"
+  puts "Self is #{self}"
+
+  def self.x
+    puts "Class Method C.x"
+    puts "Self is #{self}"
+  end
+
+  def m
+    puts "Instance method C#m"
+    puts "Self is #{self}"
+  end
+end
+
+C.x
+C.new.m
